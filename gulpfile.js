a7d89667ce6f2e9969c6cdaf1581de5d2c511ff5
Bruno Fenzl <bruno@brunofenzl.com>
2016-09-29 19:54:17 +0200

    starting project

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp          = require('gulp');
+
+// css related
+//var sass          = require('gulp-sass');
+var sourcemaps    = require('gulp-sourcemaps');
+var autoprefixer  = require('gulp-autoprefixer');
+var sassdoc       = require('sassdoc');
+var minifyCSS     = require('gulp-minify-css');
+var compass       = require('gulp-compass');
+var path          = require('path');
+var uglifycss     = require('gulp-uglifycss');
+
+//js related 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+
+var scripts = [
+    
+];
+
+var input   = 'assets/styles/main.scss';
+var output  = 'dist/styles';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+  browsers: [ 'last 2 versions' ]
+};
+
+var sassdocOptions = {
+  dest: './docs/sassdoc'
+};
+
+gulp.task('_sass', function () {
+  return gulp
+    .src(input)
+    //.pipe(sourcemaps.init())
+    .pipe(compass({
+      css: 'tmp',
+      sass: 'assets/styles',
+      sourcemap: false,
+      time: true,
+      style: 'compressed'
+    }))
+});
+
+gulp.task('_prefixer', ['_sass'], function(){
+  return gulp
+    .src('tmp/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(output))
+});
+
+gulp.task( 'build:styles', ['_prefixer'] );
+
+
+gulp.task('watch', function() {
+  return gulp
+    .watch(input, ['_prefixer'])
+});
+
+
+gulp.task('default', ['sass', 'scripts']);
